cb67cc5b94d5ea4cd3dbf0aa93e9559d9ecf1461
Andrew Ng <ayn@andrewng.com>
2008-10-09 18:07:10 -0700

    update

--- install.rb
+++ install.rb
@@ -1,8 +1,5 @@
 # Install hook code here
 puts "Copying files..."
-["javascript/jquery.clipboard.pack.js", "jquery.clipboard.swf"].each do |f|
-  dest_file = File.join(RAILS_ROOT, "public", f)
-  src_file = File.join(File.dirname(__FILE__) , f)
-  FileUtils.cp_r(src_file, dest_file)
-end
+FileUtils.cp_r(File.join(File.dirname(__FILE__), "javascript/jquery.clipboard.pack.js"), File.join(RAILS_ROOT, "public/javascripts"))
+FileUtils.cp_r(File.join(File.dirname(__FILE__), "swf/jquery.clipboard.swf"), File.join(RAILS_ROOT, "public"))
 puts "Files copied - Installation complete!"
