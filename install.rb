9d738232b6a205fbf73559fba19f76b400b043af
Andrew Ng <ayn@andrewng.com>
2009-05-08 12:33:00 -0700

    Upgraded jQuery.clipboard to 2.0.1, this disables the click to copy when you're using Flash 10. A fix for Flash 10 is in the works by its author.

--- install.rb
+++ install.rb
@@ -1,5 +1,5 @@
 # Install hook code here
 puts "Copying files..."
-FileUtils.cp_r(File.join(File.dirname(__FILE__), "javascript/jquery.clipboard.pack.js"), File.join(RAILS_ROOT, "public/javascripts"))
+FileUtils.cp_r(File.join(File.dirname(__FILE__), "javascript/jquery.clipboard.min.js"), File.join(RAILS_ROOT, "public/javascripts"))
 FileUtils.cp_r(File.join(File.dirname(__FILE__), "swf/jquery.clipboard.swf"), File.join(RAILS_ROOT, "public"))
 puts "Files copied - Installation complete!"
